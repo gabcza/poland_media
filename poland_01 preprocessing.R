e73b376f02aae3983e4ffdb6b18dee9ff3b1ff1f
Gabriela Czarnek <gczarnek@gmail.com>
2024-01-05 15:38:44 -0500

    Adding data and pre-processing code

--- poland_01 preprocessing.R
+++ poland_01 preprocessing.R
@@ -9,15 +9,16 @@ library(quanteda)
 library(quanteda.textstats)
 
 
-
 "%notin%" <- function(x, y) !(x %in% y)
 
 trust_levels <- c("Completely distrust", "Mostly distrust", "Somewhat distrust",
                   "Neither trust nor distrust",
                   "Somewhat trust", "Mostly trust", "Completely trust")
 
-dat_raw <- read.csv("/Users/ashleyblum/Downloads/Poland+Media+Project_December+29,+2023_18.27.csv",
-                      na.strings=c("-99"))
+#dat_raw <- read.csv("/Users/ashleyblum/Downloads/Poland+Media+Project_December+29,+2023_18.27.csv",
+#                      na.strings=c("-99"))
+dat_raw <- read.csv("Poland_Media_Project_January5,2024_12.29.csv",
+                    na.strings=c("-99"))
 
 questions <- as.character(dat_raw[1,])
 vars <- colnames(dat_raw)
@@ -26,7 +27,8 @@ codebook<- as.data.frame(cbind(vars, questions))
 dat_raw <- dat_raw[-(1:2),]
 
 
-dat_full <- dat_raw %>%
+dat_full <- 
+  dat_raw %>%
   filter(
     consent %in% "I consent, begin the study",
     screener_captcha %in% 15,
